c7b0a026538348148b36a4a1fe8feee5d9c2d663
nkumar15 <nir.neeraj@gmail.com>
2016-05-14 19:06:43 +0800

    factory pattern added

--- Factory/Factory/shapefactory.cpp
+++ Factory/Factory/shapefactory.cpp
@@ -1,50 +1,32 @@
 #include "shapefactory.h"
 
-class Shape
-{
-public:
-    virtual void draw() = 0;
-};
+void Circle :: draw(){
+    cout<<"Circle drawn"<<endl;
+}
 
-class Circle: public Shape
-{
-public:
-    Circle(){}
-    void draw(){
-        cout<<"Circle drawn"<<endl;
+void Square :: draw(){
+    cout<<"Square drawn";
+}
+
+Shape* ShapeFactory :: create_shape(shape_types type){
+    if( type == shape_types::Circle )
+        return new Circle();
+    else if (type == shape_types::Square)
+        return new Square();
+    else{
+        cout << "Invalid type" << endl;
+        return nullptr;
     }
-};
-
-class Square:public  Shape
-{
-public:
-    Square(){}
-    void draw(){
-        cout<<"Square drawn";
-    }
-};
-
-class ShapeFactory
-{
-public:
-    Shape * create_shape(shape_types type){
-        if( type == shape_types::Circle )
-            return new Circle();
-        else if (type == shape_types::Square)
-            return new Square();
-        else {
-            cout << "Invalid type" << endl;
-            return nullptr;
-        }
-
-    }
-};
+}
 
 void create_shape_from_factory()
 {
     shape_types  type = shape_types::Circle;
     ShapeFactory factory;
+
     Shape *shape = factory.create_shape(type);
     shape->draw();
 
+    shape  = factory.create_shape(shape_types::Square);
+    shape->draw();
 }
--- Factory/Factory/shapefactory.h
+++ Factory/Factory/shapefactory.h
@@ -4,7 +4,32 @@
 #include <iostream>
 using namespace std;
 
-enum class shape_types{Circle,Square};
+enum class shape_types{
+    Circle,
+    Square
+};
+
+class Shape{
+public:
+    virtual void draw() = 0;
+};
+
+class Circle: public Shape{
+public:
+    Circle(){}
+    void draw();
+};
+
+class Square: public Shape{
+public:
+    Square(){}
+    void draw();
+};
+
+class ShapeFactory{
+public:
+    Shape * create_shape(shape_types type);
+};
 
 void create_shape_from_factory();
 
