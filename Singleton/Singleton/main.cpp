80c61377477ffa9ce513e6d8f5d67475d195d69b
nkumar15 <nir.neeraj@gmail.com>
2016-05-14 12:53:41 +0800

    folder arrangement

---
+++ Singleton/Singleton/main.cpp
@@ -0,0 +1,38 @@
+#include "Singleton.h"
+
+int main()
+{
+    /* Singleton instance created first time */
+    Singleton *obj = get_singleton_instance();
+
+    /* Its counter value will be 0 */
+    cout<<"First reference created single instance and counter is: ";
+    obj->show();
+
+    /* First reference increments counter */
+    cout<<"First reference increments counter: ";
+    obj->increment();
+    obj->show();
+
+    /* Get singleton object. This time it instance won't be created */
+    /* Instead the previous instance referred by obj1 will be used */
+    Singleton *obj1 = get_singleton_instance();
+
+    /* obj1 will show value as  1 */
+    /* because it is the same object which obj was referring to */
+    cout<<"From second reference, counter is: ";
+    obj1->show();
+
+    /* obj and obj1 referring to same object */
+    /* so what obj has incremented obj1 will decrement it */
+    /* obj show will display value as 0 */
+    cout<<"Second reference decrements counter: ";
+    obj1->decrement();
+    obj1->show();
+
+    /* First reference will be effected by decrement done by other */
+    cout<<"Decrement has effect on first reference: ";
+    obj->show();
+
+    return 0;
+}
