d7409ac98d10bb1b80368a8dfb1770e164121ff3
neeraj <nir.neeraj@gmail.com>
2017-01-13 17:33:05 +0800

    Decorator

--- Decorator/decorator_pattern.cpp
+++ Decorator/decorator_pattern.cpp
@@ -9,13 +9,23 @@ Wrapping a gift, putting it in a box, and wrapping the box.
 
 Requirement
 ----------
+Sometimes we want to add responsibilities to individual objects, not to an entire
+class. A graphical user interface toolkit, for example, should let you add
+properties like borders or behaviors like scrolling to any user interface
+component.
+One way to add responsibilities is with inheritance. Inheriting a border from
+another class puts a border around every subclass instance. This is inflexible,
+however, because the choice of border is made statically. A client can't control
+how and when to decorate the component with a border.
+A more flexible approach is to enclose the component in another object that adds
+the border. The enclosing object is called a decorator.
 
-One common interface, Interface
-one core class , which inherits from interface
-one decorator class, which inherits from interface + creates a Interface member inside (composition of interface )
-Defined each decorator derived class from decorator class.
-Decorator derived classes implement their wrapper functionality - and - delegate to the Decorator base class.
-The client configures the type and ordering of Core and Decorator objects.
+*One common interface, Interface
+*one core class , which inherits from interface
+*one decorator class, which inherits from interface + creates a Interface member inside (composition of interface )
+*Defined each decorator derived class from decorator class.
+*Decorator derived classes implement their wrapper functionality - and - delegate to the Decorator base class.
+*The client configures the type and ordering of Core and Decorator objects.
 
 
 Examples
@@ -23,7 +33,20 @@ Examples
 The Decorator attaches additional responsibilities to an object dynamically. The ornaments that are added to pine or fir trees are examples of Decorators. Lights, garland, 
 candy canes, glass ornaments, etc., can be added to a tree to give it a festive look. The ornaments do not change the tree itself which is recognizable as a Christmas tree 
 regardless of particular ornaments used. As an example of additional functionality, the addition of lights allows one to "light up" a Christmas tree. 
-[Michael Duell, "Non-software examples of software design patterns", Object Magazine, Jul 97, p54]
+
+
+Related:
+-----------
+Adapter: A decorator is different from an adapter in that a decorator only
+changes an object's responsibilities, not its interface; an adapter will give
+an object a completely new interface.
+
+Composite : A decorator can be viewed as a degenerate composite with only
+one component. However, a decorator adds additional responsibilities—it isn't
+intended for object aggregation.
+
+Strategy : A decorator lets you change the skin of an object; a strategy
+lets you change the guts. These are two alternative ways of changing an object.
 
 */
 
